dee2317fb4b22db7ef0602103c9b6055d6fc43ca
Vardhman811 <52546204+Vardhman811@users.noreply.github.com>
2025-03-07 16:08:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,63 @@
-locals{
-gcp = "123"
+provider "google" {
+  project     = "enhanced-oasis-375017"
+  region      = "us-central1"
+#   credentials = file("./key.json")
+}
+terraform {
+  cloud {
+    organization = "VJ_ORG"
+
+    workspaces {
+      name = "tfe-sentinal"
+    }
+  }
 }
 
-output "abc" {
-value = local.gcp
+
+
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
 }
 
-output "new" {
-value = "hello"
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
